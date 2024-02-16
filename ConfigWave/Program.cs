3d5a7bd71e1b9f04c3cbecc29c3be7bdfa7b19a4
Francesco Franzini <franfrancesco@hotmail.it>
2024-02-17 00:23:41 +0100

    Setup

--- ConfigWave/Program.cs
+++ ConfigWave/Program.cs
@@ -1,10 +1,9 @@
 ﻿namespace ConfigWave
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
     }
 }
